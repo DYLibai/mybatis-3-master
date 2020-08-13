94233d037a768ad5f72011f10a6f99cd3cbc6f45
zhangsongshan <54628412@qq.com>
2020-08-13 10:06:57 +0800

    删除多余文件

--- src/test/java/com/mybatis/read/Main.java
+++ src/test/java/com/mybatis/read/Main.java
@@ -1,8 +1,12 @@
 package com.mybatis.read;
 
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.InputStream;
 
 /**
  * @Description
@@ -11,30 +15,9 @@ import java.util.Map;
  **/
 public class Main {
 
-
-    public static int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-            if (map.containsKey(complement)) {
-                return new int[] { map.get(complement), i };
-            }
-            map.put(nums[i], i);
-        }
-        throw new IllegalArgumentException("No two sum solution");
-    }
-
     public static void main(String[] args) throws IOException {
-
-        int[] nums = new int[]{2,7,11,15};
-        int target = 9;
-        long start = System.currentTimeMillis();
-        int[] ints = twoSum(nums, target);
-        long end = System.currentTimeMillis();
-        System.out.println((end-start));
-        System.out.println(ints);
         // 获取配置文件
-/*        String resource = "mybatis-config.xml";
+        String resource = "mybatis-config.xml";
         InputStream inputStream = Resources.getResourceAsStream(resource);
         // 构建SqlSessionFactory对象
         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
@@ -44,6 +27,6 @@ public class Main {
           System.out.println(user);
         } finally {
             sqlSession.close();
-        }*/
+        }
     }
 }
